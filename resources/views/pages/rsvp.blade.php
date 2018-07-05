a32620bf1cc08c6e4779bd3c2a91af45deeacd95
daviddcarr <daviddcarr13@gmail.com>
2018-07-05 15:59:00 -0500

    Add margin to bottom of RSPV form.

--- resources/views/pages/rsvp.blade.php
+++ resources/views/pages/rsvp.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="content mt-5">
+<div class="content mt-5 mb-5">
     <div class="row justify-content-center align-items-center px-3">
         <div class="col-12 col-lg-8 col-xl-6">
             <main role="main" class="inner cover">
