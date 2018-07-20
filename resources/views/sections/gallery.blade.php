d0d7b4485b653f201082c8e53ee6127afe388bba
daviddcarr <daviddcarr13@gmail.com>
2018-07-20 10:55:40 -0500

    Add gallery section back.

---
+++ resources/views/sections/gallery.blade.php
@@ -0,0 +1,4 @@
+<div id="homeFeed" class="row p-2"></div>
+<div class="row justify-content-center  pb-3">
+    <a href="https://www.instagram.com/dylan_squared" target="_blank" class="btn btn-primary">View All</a>
+</div>
