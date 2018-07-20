dc6338bb3144e2103d37d332193707d077e06c2b
daviddcarr <daviddcarr13@gmail.com>
2018-07-20 10:05:25 -0500

    Replace Instagram account with wedding account.

---
+++ resources/assets/js/components/RsvpComponent.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="form-container">
+        <form  @submit.prevent="validateBeforeSubmit" >
+            {{ csrf_field() }}
+
+            <div class="form-group">
+                <label for="name">Name<sup class='text-danger'>*</sup></label>
+                <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Enter Name" required name="name">
+            </div>
+
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" name="email">
+                <small id="emailHelp" class="form-text text-muted">Not required, if you'd like updates we'll send them via email.</small>
+            </div>
+
+            <div class="form-group">
+                <label for="family">Select Family<sup class='text-danger'>*</sup></label>
+                <select class="form-control" id="family" name="family">
+                    <option value="">Who Do You Know?</option>
+                    <option value="selby">Selby</option>
+                    <option value="carr">Carr</option>
+                    <option value="both">Love You Both!</option>
+                </select>
+            </div>
+
+
+            {{-- Guests --}}
+            <div class="form-group">
+                <label class="form-check-label">Are you bringing a guest?</label>
+            </div>
+            <div class="form-group">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="guest" id="guest1" value="yes">
+                    <label class="form-check-label" for="guest1">Yes</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="guest" id="guest2" value="no" checked>
+                    <label class="form-check-label" for="guest2">No</label>
+                </div>
+            </div>
+
+            {{-- Children --}}
+            <div class="form-group">
+                <label>Number of Children?</label>
+            </div>
+            <div class="form-group">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="num_of_children" id="num-of-children0" value="0" checked>
+                    <label class="form-check-label" for="num-of-children0">0</label>
+                </div>
+                @for ($i = 1; $i <= 4; $i++)
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="num_of_children" id="num-of-children{{ $i }}" value="{{ $i }}">
+                        <label class="form-check-label" for="num-of-children{{ $i }}">{{ $i }}</label>
+                    </div>
+                @endfor
+            </div>
+
+
+            <div class="form-group">
+                <label for="song">Recommend a song for the reception.</label>
+                <input type="text" class="form-control" id="song" aria-describedby="songHelp" name="song" placeholder="Title - Band">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Submit</button>
+
+            @include('partials.errors')
+        </form>
+
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+
+        data() {
+
+        }
+    }
+</script>
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -6,10 +6,24 @@ import Instafeed from 'instafeed.js';
 
 var feed = new Instafeed({
     get: 'tagged',
-    tagName: 'blender',
+    tagName: 'dylansquared2020',
     limit: 12,
-    clientId: 'a933d892b1f740329fd33d991fca5c32',
-    accessToken: '1485817808.a933d89.f57b0afd1bef46a989d9b5fe3844bbd4',
+    resolution: 'low_resolution',
+    sortBy: 'random',
+    clientId: 'f7f7488dab734676a35b6a1fc1b29071',
+    accessToken: '8101246636.f7f7488.27c30f13c2914241a99fb8881d38203b',
     template: '<div class="col-4 col-md-2 px-0"><div class="p-2"><a href="{{link}}" class="d-block" target="_blank"><img src="{{image}}" class="image-fluid w-100" /></a></div></div>'
 });
 feed.run();
+// var feed = new Instafeed({
+//     get: 'tagged',
+//     tagName: 'blender',
+//     limit: 12,
+//     clientId: 'a933d892b1f740329fd33d991fca5c32',
+//     accessToken: '1485817808.a933d89.f57b0afd1bef46a989d9b5fe3844bbd4',
+//     template: '<div class="col-4 col-md-2 px-0"><div class="p-2"><a href="{{link}}" class="d-block" target="_blank"><img src="{{image}}" class="image-fluid w-100" /></a></div></div>'
+// });
+// feed.run();
+
+
+// https://www.instagram.com/oauth/authorize/?client_id=f7f7488dab734676a35b6a1fc1b29071&redirect_uri=http://localhost:3000/&response_type=code&scope=basic+public_content
