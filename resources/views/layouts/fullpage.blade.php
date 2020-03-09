115cd71ca849a2e7a46056e0497344fa03ab8f91
daviddcarr <daviddcarr13@gmail.com>
2020-03-09 14:01:04 -0500

    Add public to URLs in fullpage layout file.

--- resources/views/layouts/fullpage.blade.php
+++ resources/views/layouts/fullpage.blade.php
@@ -11,8 +11,10 @@
 
         <title>The Dylans' Wedding</title>
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Roboto+Condensed|Roboto:400,400i,700&display=swap&Material+Icons" rel="stylesheet">
-        <link href="/css/app.css" rel="stylesheet">
-        <link href="/css/fullpage.min.css" rel="stylesheet">
+        {{-- <link href="/css/app.css" rel="stylesheet">
+        <link href="/css/fullpage.min.css" rel="stylesheet"> --}}
+        <link href="/public/css/app.css" rel="stylesheet">
+        <link href="/public/css/fullpage.min.css" rel="stylesheet">
     </head>
 
     <body class="text-center">
